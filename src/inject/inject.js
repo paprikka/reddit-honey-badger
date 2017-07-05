ce0f0d894df872c984a615d3fe1d8bd6ff20ef03
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2017-07-05 16:39:04 +0100

    Reload button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,64 @@
+const { assign } = Object
+
+const getNewRandomRedditURL = () => location.origin + '/r/random'
+const loadNewRandomReddit = () => location.href = getNewRandomRedditURL()
+
+const h = (tagName = 'div', properties = {}, children = []) => {
+	const el = assign(document.createElement(tagName), properties)
+
+	const iterableChildren = typeof children === 'string' ? [children] : children
+	iterableChildren.forEach(
+		child => {
+			if(typeof child === 'string') {
+				const string = child
+				const textChild = h('span')
+				textChild.innerText = string
+				el.appendChild(textChild)
+				return
+			}
+
+			el.appendChild(child)
+		}
+	)
+	
+	return el
+}
+
+
+
+class RandomRedditButton extends HTMLElement {
+	constructor() {
+		super()
+
+		this.addEventListener('click', this.onClick)
+	}
+
+	onClick() {
+		this.setAttribute('active', '')
+		loadNewRandomReddit()
+	}
+}
+
+
+
+const init = () => {
+	const reloadButton = h('random-reddit-button')
+	reloadButton.addEventListener('click', () => {
+		reloadButton.setAttribute('active', true)
+		loadNewRandomReddit()
+	})
+
+	document.body.appendChild(reloadButton)
+}
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+		clearInterval(readyStateCheckInterval)
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+		init()
+
 
 	}
-	}, 10);
-});
+	}, 10)
+})
